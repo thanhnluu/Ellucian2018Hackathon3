9ff98b74d31521c2fff8db63e76ef3671eb73fd9
Thanh Luu <tluu@USD1501278.local>
2018-06-19 09:38:57 -0400

    Added activities

---
+++ app/src/main/java/com/example/tluu/ellucian2018hackathon3/StudyGroupDetails.java
@@ -0,0 +1,13 @@
+package com.example.tluu.ellucian2018hackathon3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StudyGroupDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_study_group_details);
+    }
+}

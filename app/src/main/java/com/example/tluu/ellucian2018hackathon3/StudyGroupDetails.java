3fe6579cce48f5405e53d4d9619431981475b9b1
Thanh Luu <tluu@USD1501278.local>
2018-06-19 11:30:48 -0400

    Changes to main activity and group details

--- app/src/main/java/com/example/tluu/ellucian2018hackathon3/StudyGroupDetails.java
+++ app/src/main/java/com/example/tluu/ellucian2018hackathon3/StudyGroupDetails.java
@@ -1,7 +1,9 @@
 package com.example.tluu.ellucian2018hackathon3;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class StudyGroupDetails extends AppCompatActivity {
 
@@ -9,5 +11,18 @@ public class StudyGroupDetails extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_study_group_details);
+    }
+
+    public void addClass(View v) {
+        Intent intent = new Intent(this, CreateGroup.class);
+        startActivityForResult(intent, 1);
+    }
+
+    public void vote(View v) {
+        /*
+        * get text from view
+        * parse int and increment
+        * set view text
+        * */
     }
 }
